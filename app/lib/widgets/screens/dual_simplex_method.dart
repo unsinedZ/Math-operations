038a8c6894089790c2fc81ff07ee95a8e749f7bb
Denys Zosimovych <unsinedz.work@gmail.com>
2019-11-28 22:34:37 +0200

    Dual simplex method screen mockup

--- app/lib/widgets/screens/dual_simplex_method.dart
+++ app/lib/widgets/screens/dual_simplex_method.dart
@@ -1,6 +1,5 @@
 import 'package:app/widgets/layout/app_layout.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 class DualSimplexMethod extends StatefulWidget {
   @override
@@ -12,10 +11,38 @@ class DualSimplexMethod extends StatefulWidget {
 class _DualSimplexState extends State<DualSimplexMethod> {
   @override
   Widget build(BuildContext context) {
+    const fields = const [
+      'x1',
+      'x2',
+      'x3',
+      'x4',
+    ];
     return AppLayout(
       Form(
-        child: Column(
-          children: <Widget>[],
+        child: Row(
+          children: [
+            Text('f(x) = '),
+            ...fields.map((field) {
+              return Flexible(
+                child: Row(
+                  children: <Widget>[
+                    Flexible(
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          hintText: field,
+                        ),
+                        keyboardType: TextInputType.number,
+                        textAlign: TextAlign.right,
+                      ),
+                    ),
+                    Flexible(
+                      child: Text('* $field +'),
+                    ),
+                  ],
+                ),
+              );
+            }).toList(),
+          ],
         ),
       ),
     );
