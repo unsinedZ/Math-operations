151278aec848f644098e694b666de51d2bd26f3f
Denys Zosimovych <unsinedz.work@gmail.com>
2019-12-02 21:41:44 +0200

    Removed optional constraint

--- app/lib/widgets/primitives/common/overflow_safe_bottom_sheet_modal.dart
+++ app/lib/widgets/primitives/common/overflow_safe_bottom_sheet_modal.dart
@@ -8,38 +8,16 @@ class OverflowSafeBottomSheetModal {
   void show(BuildContext context) {
     MediaQueryData query = MediaQuery.of(context);
     bool isScrollControlled = query.orientation == Orientation.landscape;
-    showModalBottomSheet(
-      context: context,
-      isScrollControlled: isScrollControlled,
-      builder: (x) => _OptionalConstraint(
-        shouldApplyConstraint: isScrollControlled,
-        child: contentBuilder(x),
-      ),
-    );
-  }
-}
-
-class _OptionalConstraint extends StatelessWidget {
-  final Widget child;
-  final bool shouldApplyConstraint;
-
-  const _OptionalConstraint({
-    Key key,
-    @required this.child,
-    @required this.shouldApplyConstraint,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    if (!shouldApplyConstraint) return child;
-
-    MediaQueryData query = MediaQuery.of(context);
     BoxConstraints boxConstraints = BoxConstraints(
       maxHeight: query.size.height * .9,
     );
-    return ConstrainedBox(
-      constraints: boxConstraints,
-      child: child,
+    showModalBottomSheet(
+      context: context,
+      isScrollControlled: isScrollControlled,
+      builder: (x) => ConstrainedBox(
+        constraints: boxConstraints,
+        child: contentBuilder(x),
+      ),
     );
   }
 }
